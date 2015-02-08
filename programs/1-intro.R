aaf19d4e5802bd26177ba805426f6f19d9e074af
Pawel <pavopax@gmail.com>
2015-02-07 20:26:06 -0500

    add more explicit comment

--- programs/1-intro.R
+++ programs/1-intro.R
@@ -1,3 +1,6 @@
 ## your code here!
-## set working directory in RStudio with:
+## run the following command. make sure the output ends in:
+## /r-workshop-meetup-master/programs
+## if not, navigate to this directory in RStudio with:
 ## Session - Set Working Directory - Choose Directory...
+getwd()
